a3a876c0789d9ca2aaf4e2c1b823995220b9dd66
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2024-12-19 05:33:00 +0200

    Q1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Demo
+﻿using System.Text;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1)Write a program to allow the user to enter a string and print the REVERSE of it.
+            //string userInput;
+            //do
+            //{
+            //    Console.WriteLine("Enter a string please");
+            //    userInput = Console.ReadLine();
+            //} while (string.IsNullOrEmpty(userInput));
+
+            //StringBuilder result = new StringBuilder();
+            //for (int i = userInput.Length - 1; i >= 0; i--)
+            //{
+            //    result.Append(userInput[i]);
+            //}
+
+            //Console.WriteLine(result.ToString());
+            #endregion
         }
     }
 }

b9adb5493050578df9bd5985cb8dfc66940a9d18
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2024-12-19 08:16:42 +0200

    Q15

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -28,29 +28,69 @@ namespace Demo
             //do
             //{
             //    int.TryParse(Console.ReadLine(), out Number);
-       
- 
+
+
             //}
             //while (typeof(int) != Number.GetType());
 
             //StringBuilder result= new StringBuilder();
-   
+
             //int mod;
-          
+
             //while (Number > 0)
             //{  //
 
             //    mod = Number % 10;
             //    Number = Number / 10;
             //    result.Append(mod);
-            
+
             //}
             //Console.WriteLine(result);
 
 
             #endregion
 
+            #region Q15)Write a program in C# Sharp to find prime numbers within a range of numbers.
+            //int FNumberRange;
+            //int SNumberRange;
+            //do
+            //{
+            //    Console.WriteLine("Please Enter First Range Integer Number");
+            //    int.TryParse(Console.ReadLine(), out FNumberRange);
+            //    Console.WriteLine("Please Enter Second Range Integer Number");
+            //    int.TryParse(Console.ReadLine(), out SNumberRange);
 
+
+            //}
+            //while (FNumberRange < 1 || SNumberRange < FNumberRange);
+            //StringBuilder result = new StringBuilder();
+            //bool flag;
+
+            //for (int i = FNumberRange; i <= SNumberRange; i++)
+            //{
+            //    flag = true; 
+            //    if (i < 2) 
+            //        flag = false;
+             
+            //    else
+            //    {
+            //        for (int j = 2; j < i; j++)
+            //        {
+            //            if (i % j == 0)
+            //            {
+            //                flag = false;
+            //                break; 
+            //            }
+            //        }
+            //    }
+            //    if (flag)
+            //    {
+            //        result.Append(i);
+            //        result.Append(" ");
+            //    }
+            //}
+            //Console.WriteLine(result.ToString());
+            #endregion
         }
     }
 }
